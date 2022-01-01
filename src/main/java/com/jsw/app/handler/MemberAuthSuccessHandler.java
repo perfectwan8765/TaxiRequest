844d96d5a32374c11cdd399cf350b4384d538211
jsw <perfectwan8765@gmail.com>
2022-01-01 23:24:31 +0900

    UPDATE AuthSuccessHandler token handle

--- src/main/java/com/jsw/app/handler/MemberAuthSuccessHandler.java
+++ src/main/java/com/jsw/app/handler/MemberAuthSuccessHandler.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jsw.app.properties.JwtConfigProperty;
 import com.jsw.app.utils.JwtUtil;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,13 +27,16 @@ public class MemberAuthSuccessHandler implements AuthenticationSuccessHandler {
     @Autowired
     private JwtUtil jwtUtil;
 
+    @Autowired
+    private JwtConfigProperty jwtConfigProperty;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
         // create Token
         String token = jwtUtil.createToken(authentication.getName());
 
         // Add token in response
-        response.addHeader("Authorization", "token " + token);
+        response.addHeader("Authorization", String.format("%s %s", jwtConfigProperty.getPrefix(), token));
 
         Map<String, Object> resultMap = new HashMap<String, Object>();
         resultMap.put("accessToken", token);
